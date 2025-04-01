667aaacbc386fbbcce20b724794863022013468e
AFR540 <angelfdezrosado@gmail.com>
2025-04-01 18:25:53 +0200

    Responsive y barra para slector del chat

--- public/js/abrirCerrarPopup.js
+++ public/js/abrirCerrarPopup.js
@@ -1,20 +1,27 @@
 function abrirCerrarPopup() {
     const botonPopup = document.getElementById("popupButton");
+    const botonPopupOpenResponsive = document.getElementById("popupOpenResponsiveButton");
     const divPopup = document.getElementById("popupDiv");
+    const body = document.body;
 
     if (!botonPopup || !divPopup) {
         console.error("No se encontraron los elementos con los IDs proporcionados.");
         return;
     }
 
-    botonPopup.addEventListener("click", () => {
-        // Alterna entre mostrar y ocultar el popup
+    function popupAction() {
         if (divPopup.style.display === "flex") {
             divPopup.style.display = "none";
+            body.style.overflow = "auto";
         } else {
             divPopup.style.display = "flex";
+
+            body.style.overflow = "hidden";
         }
-    });
+    }
+
+    botonPopup.addEventListener("click", () => popupAction());
+    botonPopupOpenResponsive.addEventListener("click", () => popupAction());
 }
 
 // Ejecuta la función después de que el DOM esté listo
