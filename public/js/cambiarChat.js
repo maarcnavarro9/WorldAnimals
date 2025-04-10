a8bbcad5de0d4680e1a644374b23101608c99f15
AFR540 <angelfdezrosado@gmail.com>
2025-04-10 17:36:37 +0200

    Funcionalidad de cambiar de chat

---
+++ public/js/cambiarChat.js
@@ -0,0 +1,25 @@
+const chatGlobal = document.getElementById('chatGlobal');
+const chatP2P = document.getElementById('chatP2P');
+const chatIA = document.getElementById('chatIA');
+const chatGlobalButton = document.getElementById('chatGlobalButton');
+const chatP2PButton = document.getElementById('chatP2PButton');
+const chatIAButton = document.getElementById('chatIAButton');
+
+chatGlobalButton.addEventListener('click', () => {
+    chatP2P.style.display = "none";
+    chatIA.style.display = "none";
+    chatGlobal.style.display = "flex";
+});
+
+chatIAButton.addEventListener('click', () => {
+    chatP2P.style.display = "none";
+    chatGlobal.style.display = "none";
+    chatIA.style.display = "flex";
+});
+
+chatP2PButton.addEventListener('click', () => {
+    chatIA.style.display = "none";
+    chatGlobal.style.display = "none";
+    chatP2P.style.display = "flex";
+});
+
