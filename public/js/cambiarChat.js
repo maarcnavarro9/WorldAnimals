07c58bf48b1faedd57a78c3ee4f4458d06ff343f
AFR540 <angelfdezrosado@gmail.com>
2025-04-24 17:28:27 +0200

    Boton detener escritura abajo y solo cuando la IA escribe

--- public/js/cambiarChat.js
+++ public/js/cambiarChat.js
@@ -1,6 +1,6 @@
 const chatGlobal = document.getElementById('chatGlobal');
 const chatP2P = document.getElementById('chatP2P');
-const chatIA = document.getElementById('chatIA');
+const chatIA = document.getElementById('chatIAParent');
 const chatGlobalButton = document.getElementById('chatGlobalButton');
 const chatP2PButton = document.getElementById('chatP2PButton');
 const chatIAButton = document.getElementById('chatIAButton');
