fe9bbafafc7d8bde0163be73f6f6ce4a780ff59d
AFR540 <angelfdezrosado@gmail.com>
2025-05-20 18:42:30 +0200

    bloque comentado eliminado

--- public/js/script.js
+++ public/js/script.js
@@ -53,16 +53,6 @@ function initPlayer() {
         : type === "HLS" ? `./assets/videos/${currentVideo}/manifest.m3u8`
             : blockchainURL;
 
-    // const url = "";
-
-    // if (type === "DASH") {
-    //     url = `./assets/videos/${currentVideo}/manifest.mpd`;
-    // } else if (type = "HLS") {
-    //     url = `./assets/videos/${currentVideo}/manifest.m3u8`;
-    // } else {
-    //     url = "https://media.thetavideoapi.com/org_0dw11am36pv5x68scfkm93r7i4tf/srvacc_0dbmv8c5m1k237dgvq6ijpvp1/video_dxdd6prg3274ip9uwtpv0t2umh/master.m3u8";
-    // }
-
     if (type === "DASH") {
         dashPlayer = dashjs.MediaPlayer().create();
         dashPlayer.initialize(video, url, true);
